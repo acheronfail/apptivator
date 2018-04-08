5ffe8d88b4a1fe72dbcc031ec0bedc37214b302c
acheronfail <acheronfail@gmail.com>
2018-04-08 11:06:56 +1000

    minor tweaks

--- ApptivatorTests/ApplicationEntryTests.swift
+++ ApptivatorTests/ApplicationEntryTests.swift
@@ -8,20 +8,24 @@ import XCTest
 @testable import Apptivator
 
 class ApplicationEntryTests: XCTestCase {
+
+    // Since ApplicationEntry instances have some closures associated with them, it's a good idea to
+    // ensure that they're cleaned up once they go out of scope to prevent memory leaks.
     func testEntryIsDeinitialised() {
+        // Add a hook into the instance's `deinit` block.
         class TestEntry: ApplicationEntry {
             var deinitCalled: (() -> Void)?
             deinit { deinitCalled!() }
         }
 
         let expectation = XCTestExpectation(description: "deinit")
-        expectation.expectedFulfillmentCount = 1
 
-        // Create entry within block so it's cleaned up after the block.
+        // Create entry within block so it goes out of scope afterwards.
         do {
             let entry = TestEntry(url: URL(fileURLWithPath: "/Applications/Xcode.app"), config: nil)
             XCTAssert(entry != nil)
             XCTAssert(entry!.observer != nil)
+            XCTAssert(entry!.shortcutCell.shortcutValueChange != nil)
             entry!.deinitCalled = {
                 expectation.fulfill()
             }
