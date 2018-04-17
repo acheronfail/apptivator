07417068a25e236d4195031e01e86e50151d4667
acheronfail <acheronfail@gmail.com>
2018-04-17 23:27:30 +1000

    documentation updates

--- Apptivator/ApplicationState.swift
+++ Apptivator/ApplicationState.swift
@@ -4,6 +4,7 @@
 //
 
 import SwiftyJSON
+import MASShortcut
 import LaunchAtLogin
 
 @objcMembers class ApplicationState: NSObject {
@@ -13,19 +14,20 @@ import LaunchAtLogin
     // User defaults - we use it to provide some experimental overrides that haven't made their way
     // into the UI, but are being considered.
     let defaults: UserDefaults = UserDefaults.standard
-    // TODO: doc
+    // A Timer to handle the delay between keypresses in a sequence. When this runs out, then the
+    // sequence cancels and the user will have to start the sequence from the beginning.
     var timer: Timer?
-    // TODO: doc
+    // Easier access to the shared instance of MASShortcutMonitor.
     var monitor: MASShortcutMonitor! = MASShortcutMonitor.shared()
 
     // The list of application -> shortcut mappings.
     var entries: [ApplicationEntry] = []
     // Toggle for dark mode.
     var darkModeEnabled = appleInterfaceStyleIsDark()
-    // Don't fire any shortcuts if user is recording a new shortcut.
-    private var currentlyRecording = false
     // Whether or not the app should launch after login.
     private var launchAppAtLogin = LaunchAtLogin.isEnabled
+    // Don't fire any shortcuts if user is recording a new shortcut.
+    private var currentlyRecording = false
 
     // Whether or not the app is globally enabled.
     private var _isEnabled = true
@@ -39,36 +41,23 @@ import LaunchAtLogin
 
         defaults.register(defaults: [
             "leftClickToggles": false,
-            // TODO: add to about
             "sequentialShortcutDelay": 0.5,
             "matchAppleInterfaceStyle": false,
             "showPopoverOnScreenWithMouse": false
         ])
 
-        // TODO: add option in for this?
+        // Allow more shortcuts than normal.
+        // NOTE: allowing *even more* shortcuts would require a change to the MASShortcut Framework.
         MASShortcutValidator.shared().allowAnyShortcutWithOptionModifier = true
     }
 
-    // TODO: doc
-    private func keyFired(_ i: Int, _ entry: ApplicationEntry, _ shortcut: MASShortcut) {
-        if self.currentlyRecording { return }
-        if i > 0 { self.timer?.invalidate() }
-
-        // TODO: to stop this being hit at the wrong time, maybe sequences shouldn't contain
-        // subsets of other sequences..?
-        // TODO: if this is hit, then we should return early
-        // TODO: see self.isSequenceRegistered
-        if i == entry.sequence.count {
-            entry.apptivate()
-            self.registerShortcuts(atIndex: 0, last: nil)
-        } else {
-            let last = (shortcut.keyCode, shortcut.modifierFlags)
-            self.registerShortcuts(atIndex: i, last: last)
-        }
+    // Disable all shortcuts when the user is recording a shortcut.
+    func onRecordingChange<Value>(_ view: MASShortcutView, _ change: NSKeyValueObservedChange<Value>) {
+        currentlyRecording = view.isRecording
     }
 
-    // TODO: doc
-    // Should be called whenever a shortcut changes value
+    // This resets the shortcut state to its initial setting. This should be called whenever a
+    // an ApplicationEntry updates its sequence.
     func registerShortcuts() {
         self.registerShortcuts(atIndex: 0, last: nil)
     }
@@ -86,8 +75,8 @@ import LaunchAtLogin
         })
     }
 
-    // TODO: doc
-    // TODO: doc - probably shouldn't be called, but need it for tests
+    // Only register the shortcuts that are expected. Ideally this should be a private function, but
+    // we need to expose it here s in order to write tests for its behaviour.
     func registerShortcuts(atIndex index: Int, last: (UInt, UInt)?) {
         self.unregisterShortcuts()
 
@@ -95,7 +84,7 @@ import LaunchAtLogin
         self.entries.forEach({ entry in
             if index < entry.sequence.count {
                 let shortcut = entry.sequence[index].shortcutValue!
-                // If this is the first shortcut (index = 0), then bind all the first shortcut keys.
+                // If this is the first shortcut (index == 0) then bind all the first shortcut keys.
                 if index == 0 {
                     if !monitor.isShortcutRegistered(shortcut) {
                         monitor.register(shortcut, withAction: { self.keyFired(1, entry, shortcut) })
@@ -104,7 +93,7 @@ import LaunchAtLogin
                 }
 
                 // If this is a sequential shortcut (index > 0), then only bind the next shortcuts
-                // whose previous shortcut was hit.
+                // at the given index, whose previous shortcut was hit.
                 let (lastKeyCode, lastModifierFlags) = last!
                 let prev = entry.sequence[index - 1].shortcutValue!
                 if prev.keyCode == lastKeyCode && prev.modifierFlags == lastModifierFlags {
@@ -126,9 +115,30 @@ import LaunchAtLogin
         }
     }
 
-    // TODO: this
+    // This is called when a key is hit in a sequence of shortcuts. If it's the last shortcut, it
+    // will activate the app, otherwise it will just advance the sequence along.
+    private func keyFired(_ i: Int, _ entry: ApplicationEntry, _ shortcut: MASShortcut) {
+        if self.currentlyRecording { return }
+        if i > 0 { self.timer?.invalidate() }
+
+        // Last shortcut in sequence: apptivate and reset shortcut state.
+        if i == entry.sequence.count {
+            entry.apptivate()
+            self.registerShortcuts(atIndex: 0, last: nil)
+        } else {
+            // Advance shortcut state with last shortcut and the number of shortcuts hit.
+            let last = (shortcut.keyCode, shortcut.modifierFlags)
+            self.registerShortcuts(atIndex: i, last: last)
+        }
+    }
+
+    // This checks the given sequences to see if it conflicts with another sequence. Shortcut
+    // sequences must have unique prefixes, so that each one can be distinguished from another.
+    // See `SequenceViewController.showConflictingEntry()`.
     func checkForConflictingSequence(_ otherSequence: [MASShortcutView], excluding otherEntry: ApplicationEntry?) -> ApplicationEntry? {
-        if otherSequence.count == 0 { return nil }
+        // It doesn't make sense to call this function with an empty sequence.
+        assert(otherSequence.count > 0, "tried to check sequence with count == 0")
+
         return self.entries.first(where: { entry in
             if entry.sequence.count == 0 || entry === otherEntry { return false }
             var wasConflict = true
@@ -141,10 +151,6 @@ import LaunchAtLogin
 
             return wasConflict
         })
-    }
-
-    func onRecordingChange<Value>(_ view: MASShortcutView, _ change: NSKeyValueObservedChange<Value>) {
-        currentlyRecording = view.isRecording
     }
 
     // Loads the app state (JSON) from disk - if the file exists, otherwise it does nothing.
